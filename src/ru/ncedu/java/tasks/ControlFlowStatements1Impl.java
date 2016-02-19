0776795e0ec136b08d4b5fc744fcbae5a7c9c134
ayulit <litwinoff@gmail.com>
2016-02-19 22:10:18 +0300

    Created interface ControlFlowStatements1.java and then created its implementation

---
+++ src/ru/ncedu/java/tasks/ControlFlowStatements1Impl.java
@@ -0,0 +1,39 @@
+package ru.ncedu.java.tasks;
+
+public class ControlFlowStatements1Impl implements ControlFlowStatements1 {
+
+	public ControlFlowStatements1Impl() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public float getFunctionValue(float x) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String decodeWeekday(int weekday) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int[][] initArray() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getMinValue(int[][] array) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public BankDeposit calculateBankDeposit(double P) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
