e05749c8e545d93b2e65b334fcfffa97de56d61f
ayulit <litwinoff@gmail.com>
2016-02-19 23:29:24 +0300

    Implemented methods getFunctionValue, decodeWeekday, initArray, getMinValue. Runner class modified to test implemented methods.

--- src/ru/ncedu/java/tasks/ControlFlowStatements1Impl.java
+++ src/ru/ncedu/java/tasks/ControlFlowStatements1Impl.java
@@ -8,26 +8,74 @@ public class ControlFlowStatements1Impl implements ControlFlowStatements1 {
 
 	@Override
 	public float getFunctionValue(float x) {
-		// TODO Auto-generated method stub
-		return 0;
+		// TODO Auto-generated method stub		
+		if (x>0) 
+		{
+			return (float) (2*Math.sin(x));
+		} else 	{
+			return 6-x;
+		}				
 	}
 
 	@Override
 	public String decodeWeekday(int weekday) {
 		// TODO Auto-generated method stub
-		return null;
+	    String weekdayString;	    
+        switch (weekday) {
+            case 1:  weekdayString = "Monday";
+                     break;
+            case 2:  weekdayString = "Tuesday";
+                     break;
+            case 3:  weekdayString = "Wednesday";
+                     break;
+            case 4:  weekdayString = "Thursday";
+                     break;
+            case 5:  weekdayString = "Friday";
+                     break;
+            case 6:  weekdayString = "Saturday";
+                     break;
+            case 7:  weekdayString = "Sunday";
+                     break;            
+            default: weekdayString = "Invalid weekday";
+                     break;
+        }		
+		return weekdayString;
 	}
 
 	@Override
 	public int[][] initArray() {
 		// TODO Auto-generated method stub
-		return null;
+		int[][] array = new int[8][5];
+		
+		for (int i = 0; i < array.length; i++)
+		{
+			for (int j = 0; j < array[j].length; j++)
+			{
+				array[i][j] = i*j;
+			}	
+		}
+		
+		return array;
 	}
 
 	@Override
 	public int getMinValue(int[][] array) {
 		// TODO Auto-generated method stub
-		return 0;
+		
+		int min = array[0][0];
+		
+		for (int i = 0; i < array.length; i++)
+		{
+			for (int j = 0; j < array[j].length; j++)
+			{
+				if(array[i][j] < min)
+				{
+                    min = array[i][j];
+				}
+			}	
+		}
+		
+		return min;
 	}
 
 	@Override
--- src/ru/ncedu/java/tasks/Runner.java
+++ src/ru/ncedu/java/tasks/Runner.java
@@ -1,5 +1,7 @@
 package ru.ncedu.java.tasks;
 
+import java.util.Random;
+
 public class Runner {
 
 	/**
@@ -7,8 +9,50 @@ public class Runner {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello, World!");
+		System.out.println("Hello, World!"); 
+		
+		ControlFlowStatements1 object = new ControlFlowStatements1Impl();
+		
+		System.out.println(object.getFunctionValue((float) (3.14/2))); // 2
+		System.out.println(object.getFunctionValue(0)); // 6
+		System.out.println(object.getFunctionValue(-2)); // 8
+		
+		System.out.println(object.decodeWeekday(4)); // Thursday
+		
+        // Prints 2D array
+		for(int[] row : object.initArray()) {
+            printRow(row);
+        }
+		
+		System.out.println("=========================");
+		
+		// Random integer between 1(inclusive) and 100 (exclusive)
+		Random random = new Random();
+        int Low = 1;
+        int High = 100;
+                
+        int twoDm[][]= new int[7][5];
+        
+        for(int i=0;i<7;i++) {
+            for(int j=0;j<5;j++) {
+                twoDm[i][j]=random.nextInt(High-Low) + Low;        
+            }
+        }
+        for(int[] row : twoDm) {
+            printRow(row);
+        }
+        System.out.println("Min = " + object.getMinValue(twoDm));
+		
 
 	}
+	
+    // printing 2D array
+	public static void printRow(int[] row) {
+        for (int i : row) {
+            System.out.print(i);
+            System.out.print("\t");
+        }
+        System.out.println();
+    }
 
 }
