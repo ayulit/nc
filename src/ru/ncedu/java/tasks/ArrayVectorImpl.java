b0a951f7ce8c458a4a69c8d0add6080c95c4dacd
ayulit <litwinoff@gmail.com>
2016-04-02 22:08:08 +0300

    Initial commit: created interface ArrayVector.java and then created its implementation CloneNotSupportedException in throws clause of Object.clone() is not compatible fixed

---
+++ src/ru/ncedu/java/tasks/ArrayVectorImpl.java
@@ -0,0 +1,85 @@
+package ru.ncedu.java.tasks;
+
+public class ArrayVectorImpl implements ArrayVector {
+
+	public ArrayVectorImpl() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public ArrayVectorImpl clone() {
+		return null;
+	}
+
+	@Override
+	public void set(double... elements) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public double[] get() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getSize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void set(int index, double value) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public double get(int index) throws ArrayIndexOutOfBoundsException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getMax() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getMin() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void sortAscending() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mult(double factor) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public ArrayVector sum(ArrayVector anotherVector) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double scalarMult(ArrayVector anotherVector) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getNorm() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
