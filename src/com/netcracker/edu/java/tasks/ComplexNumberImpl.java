aec4bc996469a33f26bd56779e0c8cb2f0d34bcd
ayulit <litwinoff@gmail.com>
2016-04-03 01:32:19 +0300

    Initial commit: created interface ComplexNumber and then created its implementation

---
+++ src/com/netcracker/edu/java/tasks/ComplexNumberImpl.java
@@ -0,0 +1,81 @@
+package com.netcracker.edu.java.tasks;
+
+public class ComplexNumberImpl implements ComplexNumber {
+
+	public ComplexNumberImpl() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public ComplexNumber clone() throws CloneNotSupportedException {
+		
+		//ArrayVector copy = new ArrayVectorImpl(vec.clone());	
+		return null;
+	}
+
+	@Override
+	public double getRe() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getIm() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isReal() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void set(double re, double im) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void set(String value) throws NumberFormatException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public ComplexNumber copy() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int compareTo(ComplexNumber other) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void sort(ComplexNumber[] array) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public ComplexNumber negate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ComplexNumber add(ComplexNumber arg2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ComplexNumber multiply(ComplexNumber arg2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
