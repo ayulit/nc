3d7d8ed0aa95c36b3476d84c282fc65e96b1f5cd
ayulit <litwinoff@gmail.com>
2016-04-03 20:02:44 +0300

    Initial commit

---
+++ src/com/netcracker/edu/java/tasks/TreeNodeImpl.java
@@ -0,0 +1,101 @@
+package com.netcracker.edu.java.tasks;
+
+import java.util.Iterator;
+
+public class TreeNodeImpl implements TreeNode {
+
+	public TreeNodeImpl() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public TreeNode getParent() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setParent(TreeNode parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public TreeNode getRoot() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isLeaf() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getChildCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Iterator<TreeNode> getChildrenIterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addChild(TreeNode child) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean removeChild(TreeNode child) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isExpanded() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setExpanded(boolean expanded) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Object getData() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setData(Object data) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getTreePath() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TreeNode findParent(Object data) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TreeNode findChild(Object data) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
