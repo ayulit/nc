d16acdf1f0e862c1a6b225c92882f0d7bc9ca468
ayulit <litwinoff@gmail.com>
2016-04-03 03:42:17 +0300

    Add printFormatted static method in Runner class

--- src/com/netcracker/edu/java/tasks/Runner.java
+++ src/com/netcracker/edu/java/tasks/Runner.java
@@ -2,22 +2,23 @@ package com.netcracker.edu.java.tasks;
 
 public class Runner {
 
+	public static void printFormatted(ComplexNumber z) {
+		System.out.println("z=" + z.getRe() +(z.getIm() < 0 ? "" : "+") + z.getIm() + "i" );		
+	}
+	
 	public static void main(String[] args) {
 
 		System.out.println("Hello, World!");
 		
 		ComplexNumber z = new ComplexNumberImpl();		
-		System.out.println("z=" + z.getRe() + "+i*" + z.getIm());
+		//System.out.println("z=" + z.getRe() +(z.getIm() < 0 ? "" : "+") + z.getIm() + "i" );
+		printFormatted(z);
 		z = new ComplexNumberImpl(2,-3);
-		System.out.println("z=" + z.getRe() + "+i*" + z.getIm());
+		printFormatted(z);
 		System.out.println("z is " + (z.isReal() ? "real" : "complex"));
 		z.set(5,0);
-		System.out.println("z=" + z.getRe() + "+i*" + z.getIm());
-		System.out.println("z is " + (z.isReal() ? "real" : "complex"));
-		
-		
-		
-		
+		printFormatted(z);
+		System.out.println("z is " + (z.isReal() ? "real" : "complex"));		
 		
 	}
 
